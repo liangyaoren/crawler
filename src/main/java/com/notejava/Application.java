afeaec5a3cffad92d8a76449a90f6052ef7df643
liyren <1596430288@qq.com>
2022-01-10 18:05:25 +0800

    Update Application.java

--- src/main/java/com/notejava/Application.java
+++ src/main/java/com/notejava/Application.java
@@ -12,7 +12,6 @@ public class Application {
 
     public static void main(String[] args) throws IOException, InterruptedException {
         SpringApplication.run(Application.class, args);
-        Bmw.test();
-        //Bilibili.test();
+        Bilibili.test();
     }
 }
